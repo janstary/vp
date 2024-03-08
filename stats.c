4074d807589b3d7fe3f26761896e1f046e670524
Jan Starý <hans@stare.cz>
2024-03-08 17:56:27 +0100

    the covariance matrix of formants
    This is the first step in the Principal Component Analysis
    of the formants data, which will reveal the "best" ellipse.

---
+++ stats.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include "vowel.h"
+#include "group.h"
+
+float
+cov(struct group* g, int i, int j)
+{
+	float sum;
+	struct vowel* v;
+	for (sum = 0, v = g->head; v ; v = v->next)
+		sum += (v->F[i] - g->grav[i]) * (v->F[j] - g->grav[j]);
+	return sum / g->size;
+}
+
+void
+matrix(struct group* g)
+{
+	int i, j;
+	float M[2][2];
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 2; j++) {
+			M[i][j] = cov(g, i, j);
+			printf("M(%d, %d) = %f\n", i, j, M[i][j]);
+		}
+	}
+}
---
+++ stats.h
@@ -0,0 +1,6 @@
+#ifndef _STATS_H_
+#define _STATS_H_
+
+#include "group.h"
+
+#endif
